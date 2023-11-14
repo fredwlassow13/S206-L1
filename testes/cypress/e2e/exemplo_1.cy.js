57bd56d4afeea59869a1a369b2122ec029a5d19b
Fredwlassow <Fredwlassow13@gmail.com>
2023-11-13 21:39:11 -0300

    gerando Relatorio

--- testes/cypress/e2e/exemplo_1.cy.js
+++ testes/cypress/e2e/exemplo_1.cy.js
@@ -1,5 +1,64 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+/// <reference types= "cypress"/>
+
+describe ('criando cenario de teste para o site globalsqa', () => {
+
+    it('Caso teste: Registrando um usuário no site com sucesso', () => {
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type('inatel')
+        cy.get('#Text1').type('inatel')
+        cy.get('#username').type('inatel')
+        cy.get('#password').type('inatel')
+        cy.get('.btn-primary').click()
+        cy.get('.ng-binding').should('contain.text','Registration successful')
+    })
+
+    it('Caso teste: Registrando um usuário no site com erro', () => {
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/register')
+        cy.get('#firstName').type('inatel')
+        cy.get('#Text1').type('inatel')
+        cy.get('#username').type('inatel')
+        cy.get('#password').type('inatel')
+        cy.get('#password').clear()
+        cy.get('.has-error > .help-block').should('have.text','Password is required')
+        cy.get('.btn-primary').should('be.disabled')
+    })
+
+   it('Caso de teste: Realizando login com Sucesso', () => {
+        let info = criarUsuario()
+        cy.get('#username').type(info[0])
+        cy.get('#password').type(info[1])
+        cy.get('.btn-primary').click()
+        cy.get('h1.ng-binding').should('contain.text',info[0])
+   })
+
+   it('Caso de teste: deletando usuario com sucesso', () => {
+    let info = criarUsuario()
+    cy.login(info[0], info[1])
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').click()
+    cy.login(info[0], info[1])
+    cy.get('.ng-binding').should('have.text', 'Username or password is incorrect')
+   })
 })
+
+   function criarUsuario(){
+
+        let horas = new Date().getHours().toString();
+        let minutos = new Date().getMinutes().toString();
+        let seg = new Date().getSeconds().toString();
+        let user = horas + minutos + seg + 'Id'
+        let senha = horas + minutos + seg + 'senha'
+        let userInfo = [user, senha]
+
+        cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+        cy.get('.btn-link').click()
+        cy.get('#firstName').type(user)
+        cy.get('#Text1').type(user)
+        cy.get('#username').type(user)
+        cy.get('#password').type(senha)
+        cy.get('.btn-primary').click()
+        cy.get('.ng-binding').should('contain.text','Registration successful')
+
+        return userInfo
+   }
--- testes/cypress/support/commands.js
+++ testes/cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, senha) => {
+    cy.get('#username').type(username)
+    cy.get('#password').type(senha)
+    cy.get('.btn-primary').click()
+
+})
